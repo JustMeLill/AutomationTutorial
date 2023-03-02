be871fd88ed9f37d860b1a71ac33388a70edf0c6
JustMeLill <dutescu_lilly@yahoo.com>
2023-03-02 19:14:46 +0200

    finish ci/cd

--- src/test/java/SharedData/SharedData.java
+++ src/test/java/SharedData/SharedData.java
@@ -1,7 +1,9 @@
 package SharedData;
 
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -17,21 +19,35 @@ public class SharedData {
 
 
     public void Setup(){
-        //Setam driverul pentru browser, utilizand un set de key si value;
-        //Daca vrem alt browser, punem driverul de browser, si adaugam valoarea path-ului de la driver si inlocuim in key
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/Driver/chromedriver.exe");
+
+        String cicd = System.getProperty("ci_cd");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        if(Boolean.parseBoolean(cicd)){
+            chromeOptions.addArguments("--headless");
+        }
+        else {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/Driver/chromedriver.exe");
+            //Setam driverul pentru browser, utilizand un set de key si value;
+            //Daca vrem alt browser, punem driverul de browser, si adaugam valoarea path-ului de la driver si inlocuim in key
+        }
+        //Setez rezolutia dorita
+        chromeOptions.addArguments("--window-size=1200,1080");
+        chromeOptions.addArguments("--no-sandbox");
+        //Setez cum vreau sa se comporte cand se incarca o pagina - strategia paginii (None, Eager, Normal)
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
 
         //Deschidem un browser de chrome
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(chromeOptions);
 
         //Facem driverul in format maximize
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
 
         // Accesam un URL specific
         driver.get("https://demo.automationtesting.in/Index.html");
 
         //Folosim metoda wait implicit
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
 
 
     }
